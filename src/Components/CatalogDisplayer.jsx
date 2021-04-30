036e3136dce40af18dce8ad800b336fd5a741f69
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2021-04-30 13:10:52 -0300

    transform catalogo page into a component

--- src/Components/CatalogDisplayer.jsx
+++ src/Components/CatalogDisplayer.jsx
@@ -1,35 +1,13 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby'
 import { ProductCard } from '../Components'
 import * as styles from '../styles/CatalogDisplayer.module.css'
 
-const CatalogDisplayer = () => {
-    const { allStrapiProdutos: products } = useStaticQuery(graphql`
-    {
-      allStrapiProdutos {
-        products:nodes {
-          titulo
-          preco
-          preco_comparativo
-          imagem_de_capa {
-            childImageSharp {
-                fluid(maxWidth: 380, maxHeight: 380) {
-                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
-                }
-            }
-          }
-        }
-      }
-    }
-  `)
-
-    return (
-        <div className={styles.catalogContainer}>
-            {products.products.map((prod, key) => (
-                <ProductCard product={prod} key={key}/>
-            ))}
-        </div>
-    );
-}
+const CatalogDisplayer = ({products}) => (
+    <div className={styles.catalogContainer}>
+        {products.products.map((prod, key) => (
+            <ProductCard product={prod} key={key}/>
+        ))}
+    </div>
+)
  
 export default CatalogDisplayer;
--- src/pages/catalogo.jsx
+++ src/pages/catalogo.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 import DefaultTemplate from '../templates/DefaultTemplate'
 import { CatalogDisplayer } from '../Components'
 
-const catalogo = () => {
+
+const Catalogo = () => {
+    const { allStrapiProdutos: products } = useStaticQuery(graphql`
+        {
+            allStrapiProdutos {
+                    products:nodes {
+                    titulo
+                    preco
+                    preco_comparativo
+                    imagem_de_capa {
+                        childImageSharp {
+                            fluid(maxWidth: 380, maxHeight: 380) {
+                                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <DefaultTemplate>
-            <CatalogDisplayer />
+            <CatalogDisplayer products={products}/>
         </DefaultTemplate>
     )
 }
  
-export default catalogo;
+export default Catalogo;
