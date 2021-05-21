8a4a36dead2374f7137bf04eeb47f6a193b2f54d
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2021-05-21 11:37:48 -0300

    add env variable to gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.GATSBY_BACKEND_URL,
         queryLimit: 1000, // Default to 100
         contentTypes: [`produtos`, `carrossels`, `Assets`, `categorias`],
         singleTypes: [],
